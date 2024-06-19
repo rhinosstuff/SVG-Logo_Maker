45f6acec10ec87797977183aed57837926da6475
rhinosstuff <55716738+rhinosstuff@users.noreply.github.com>
2024-06-19 12:03:14 -0600

    added dom structure and basic inquire and shape content

---
+++ README.md
@@ -0,0 +1,40 @@
+
+  # SVG-Logo_Maker
+  [![Static Badge](https://img.shields.io/badge/license-MIT-blue.svg)](./LICENSE)
+
+  ## Description
+  This is my week 10-Challenge. In this challenge I am supposed to create a SVG logo from scratch.
+
+  ## Table of Contents 
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+  To install necessary dependencies, run the following command:
+  ```
+  npm i
+  ```
+  ## Usage
+  Soon to be discovered. 
+
+  ## License
+  This project is licensed under the **[MIT](./LICENSE)** license.
+
+  ## Contributing
+  N/A
+
+  ## Tests
+  To run tests, run the following command:
+  ```    
+  npm test
+  ```
+  ## Questions
+  If you have any questions about the repo, open an issue or contact me directly at **rhinosstuff@gmail.com**.
+  
+  You can explore my other projects on GitHub **[rhinosstuff](https://github.com/rhinosstuff)**.
+  
+  
---
+++ index.js
@@ -0,0 +1,42 @@
+// Includes file system, inquirer and local shapes packages.
+const fs = require('fs')
+const inquirer = require('inquirer')
+const shapes = require('./lib/shapes')
+
+// Creates & writes to logo.svg
+function writeToFile(fileName, data) {
+  const logo = shapes(data)
+  
+  fs.writeFile(fileName, logo, (err) =>
+    err ? console.log(err) : console.log('Success!')
+  )
+}
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: `Enter up to 3 characters to represent your company name - Logo Text:`,
+      name: 'text',
+    },
+    {
+      type: 'input',
+      message: `Enter a color or hexadecimal number - Text Color:`,
+      name: 'textColor',
+    },
+    {
+      type: 'list',
+      message: `Logo Shape:`,
+      choices: ['circle', 'triangle', 'square'],
+      name: 'shape',
+    },
+    {
+      type: 'input',
+      message: `Enter a color or hexadecimal number - Shape Color:`,
+      name: 'shapeColor',
+    }
+  ])
+  .then((data) => {
+    const filename = `./examples/${data.text}_logo.svg`
+    writeToFile(filename, data)
+  })
