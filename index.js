c08966421232ba7d545c6881a65a4eff71a99c8b
rhinosstuff <55716738+rhinosstuff@users.noreply.github.com>
2024-06-25 21:32:54 -0600

    Added shape color tests, refined index code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Includes file system, inquirer and local shapes packages.
 const fs = require('fs')
 const inquirer = require('inquirer')
-const shapes = require('./lib/shapes')
+const generateLogo = require('./lib/generateLogo')
 
 // Creates & writes to logo.svg
 function writeToFile(fileName, data) {
-  const logo = shapes(data)
-  
+  const logo = generateLogo(data)
+
   fs.writeFile(fileName, logo, (err) =>
-    err ? console.log(err) : console.log(`Generated ${data.text}_logo.svg`)
+    err ? console.log(err) : console.log(`Generated ${data.text}.svg`)
   )
 }
 
@@ -16,27 +16,36 @@ inquirer
   .prompt([
     {
       type: 'input',
-      message: `Enter up to 3 characters to represent your company name - Logo Text:`,
+      message: `Logo Text: (Max of 3 Characters)`,
       name: 'text',
+      validate: function(input) {
+        return input.length < 4 && input.length > 0 ? true : 'Please enter text - max of 3 characters'
+      }
     },
     {
       type: 'input',
-      message: `Enter a color or hexadecimal number - Text Color:`,
+      message: `Text Color: (Color Keyword or Hexadecimal)`,
       name: 'textColor',
+      validate: function(input) {
+        return input ? true : 'Please enter a color'
+      }
     },
     {
       type: 'list',
       message: `Logo Shape:`,
-      choices: ['circle', 'triangle', 'square'],
+      choices: ['circle', 'square', 'triangle'],
       name: 'shape',
     },
     {
       type: 'input',
-      message: `Enter a color or hexadecimal number - Shape Color:`,
+      message: `Shape Color: (Color Keyword or Hexadecimal)`,
       name: 'shapeColor',
+      validate: function(input) {
+        return input ? true : 'Please enter a color'
+      }
     }
   ])
   .then((data) => {
-    const filename = `./examples/${data.text}_logo.svg`
+    const filename = `./examples/${data.text}.svg`
     writeToFile(filename, data)
   })
