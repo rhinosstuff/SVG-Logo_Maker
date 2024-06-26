5027d442bbed44f2deb80c5276cf153ecb0537d5
rhinosstuff <55716738+rhinosstuff@users.noreply.github.com>
2024-06-26 10:42:54 -0600

    implemented tests, updated README.md

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
   [![Static Badge](https://img.shields.io/badge/license-MIT-blue.svg)](./LICENSE)
 
   ## Description
-  This is my week 10-Challenge. In this challenge I am supposed to create a SVG logo from scratch.
+  This is my week 10-Challenge. In this challenge I was to create a simple SVG logo generator from scratch. It will take in a few questions from the command line using "inquirer" then generate a svg file based on user input. I also needed to implement "jest" for testing the application.
 
   ## Table of Contents 
   * [Installation](#installation)
@@ -19,7 +19,21 @@
   npm i
   ```
   ## Usage
-  Soon to be discovered. 
+  To start the app, run the following command:
+  >`node index.js`
+  >
+  >You will then be prompted with a series of questions.
+  >
+  >***Logo Text:*** - takes a max of 3 characters and a min of 1\
+  >***Text Color:*** - takes a Color Keyword or Hexadecimal - e.g. ( white or #FFFFFF )\
+  >***Logo Shape:*** - select from list\
+  >***Shape Color:*** - takes a Color Keyword or Hexadecimal - e.g. ( white or #FFFFFF )
+  >
+  >*** **[Video Link Demonstration](https://watch.screencastify.com/v/1Z983LE1x0dIhTCOJxSV)** ***
+
+  Image examples of what to expect.
+  <img src="images/svgLogo.png" width="800">\
+  <img src="images/npmTest.png" width="600">
 
   ## License
   This project is licensed under the **[MIT](./LICENSE)** license.
--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-// Includes file system, inquirer and local shapes packages.
+// Gets file system & inquirer packages. And local generateLogo.js.
 const fs = require('fs')
 const inquirer = require('inquirer')
 const generateLogo = require('./lib/generateLogo')
 
-// Creates & writes to logo.svg
 function writeToFile(fileName, data) {
+  // Creates the html for the logo
   const logo = generateLogo(data)
 
+  // Writes the logo html to the supplied path and file name
   fs.writeFile(fileName, logo, (err) =>
     err ? console.log(err) : console.log(`Generated ${data.text}.svg`)
   )
 }
 
+// prompts the user through a series of questions
 inquirer
   .prompt([
     {
       type: 'input',
       message: `Logo Text: (Max of 3 Characters)`,
       name: 'text',
+      // Checks that at least 1 character is entered but no more than 3
       validate: function(input) {
-        return input.length < 4 && input.length > 0 ? true : 'Please enter text - max of 3 characters'
+        return input.length < 4 && input.length > 0 ? true : 'Please enter some text - max of 3 characters'
       }
     },
     {
       type: 'input',
       message: `Text Color: (Color Keyword or Hexadecimal)`,
       name: 'textColor',
+      // Makes sure the user enters a color
       validate: function(input) {
         return input ? true : 'Please enter a color'
       }
@@ -40,12 +44,14 @@ inquirer
       type: 'input',
       message: `Shape Color: (Color Keyword or Hexadecimal)`,
       name: 'shapeColor',
+      // Makes sure the user enters a color
       validate: function(input) {
         return input ? true : 'Please enter a color'
       }
     }
   ])
   .then((data) => {
+    // Provides the directory path and creates .svg file name based off the user input text 
     const filename = `./examples/${data.text}.svg`
     writeToFile(filename, data)
   })
